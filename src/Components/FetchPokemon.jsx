bd2d895a38b42d85e5efd3e2e777ceff6bbf5980
RichardTCrocker <richardtcrocker@gmail.com>
2023-08-07 14:56:35 -0400

    first commit

---
+++ src/Components/FetchPokemon.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+
+const FetchPokemon = () => {
+    const [pokemon, setPokemon] = useState([]);
+
+    const getPoke = () => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+        .then(response => {
+            return response.json()
+        }
+        )
+        .then(responseJson => {
+            setPokemon(responseJson.results)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    return (
+        <div>
+            <h1>Get Pokemon</h1>
+                <ul>
+                    {pokemon.map((pokemon, idx) =>
+                    <li key={idx}>{pokemon.name}</li>
+                    )}
+                    
+                </ul>
+            <button onClick={getPoke}>Get All Pokemon</button>
+        </div>
+    )
+}
+
+export default FetchPokemon
